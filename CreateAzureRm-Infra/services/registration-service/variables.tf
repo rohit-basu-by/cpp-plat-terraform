9322be1de6434fc6d40ed271c04074c8c98e1f8d
Rohit Basu <rohit.basu@blueyonder.com>
2020-06-25 16:22:31 +0530

    second draft

--- CreateAzureRm-Infra/services/registration-service/variables.tf
+++ CreateAzureRm-Infra/services/registration-service/variables.tf
@@ -8,16 +8,6 @@ terraform {
   required_version = ">= 0.12.6"
 }
 
-variable "subscription_id" {
-  description = "Azure subscription Id."
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure tenant Id."
-  type        = string
-}
-
 variable "rg_infr_name" {
   description = "infra resource group name."
   type        = string
