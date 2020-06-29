e87b562f885da9b456525043b9bd700cdd6e4940
Rohit Basu <rohit.basu@blueyonder.com>
2020-06-29 13:02:44 +0530

    sample #13

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "cosmos_connection" {
+  description = "Generic Cosmos Endpoint"
+  value       = module.Create-Infrastructure.cosmos_connection
+}
+
+output "cosmos_key" {
+  description = "Generic Cosmos Key"
+  value       = module.Create-Infrastructure.cosmos_key
+}
+
+output "app_service_plan" {
+  description = "Generic App Service Plan"
+  value       = module.Create-Infrastructure.app_service_plan
+}
+
+output "storage_connection_string" {
+  description = "Generic Storage Connection String"
+  value       = module.Create-Infrastructure.storage_connection_string
+}
