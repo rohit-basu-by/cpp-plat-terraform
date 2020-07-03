63a6cb10ea3ff8e9ebe478d0a4949f045aba03aa
Rohit Basu <rohit.basu@blueyonder.com>
2020-07-03 12:40:51 +0530

    Notification hub endpoint

--- module/Az-NotificationHub/outputs.tf
+++ module/Az-NotificationHub/outputs.tf
@@ -8,10 +8,9 @@ output "notificationHubs" {
         for rule in azurerm_notification_hub_authorization_rule.main :
         rule.name => {
           name                 = rule.name
-          hub_connection_string   = "Endpoint=sb://${rule.name+}.servicebus.windows.net/;SharedAccessKeyName=${rule.name};SharedAccessKey=${rule.primary_connection_string}"
+          hub_connection_string   = format("Endpoint=sb://%s.servicebus.windows.net/;SharedAccessKeyName=%s;SharedAccessKey=%s",rule.name,rule.name,rule.primary_access_key)
           primary_access_key   = rule.primary_access_key
           secondary_access_key = rule.secondary_access_key
-          //secondary_connection_string = rule.secondary_connection_string
         } if hub.name == rule.name
       }
     }
